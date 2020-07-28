bc2ea9e0c81cc68a445522c2f628334d6ccdcd02
Daniel Lee <dlee@sheetmusicplus.com>
2020-07-28 00:03:57 -0700

    Fix form labels and other small changes

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,114 +1,126 @@
 <template>
   <form>
     <div class="field">
-      <label class="label">City</label>
-      <div class="control">
-        <input
-          v-model="form['city']"
-          v-on:keydown.13.prevent="parseFormFields"
-          type="text"
-          placeholder="City"
-          class="input is-small"
-        >
-      </div>
+      <label class="label">
+        City
+        <div class="control">
+          <input
+            v-model="form['city']"
+            v-on:keydown.13.prevent="parseFormFields"
+            type="text"
+            placeholder="City"
+            class="input is-small"
+          >
+        </div>
+      </label>
     </div>
 
     <div class="field">
-      <label class="label">Country</label>
-      <div class="control">
-        <input
-          v-model="form['country']"
-          v-on:keydown.13.prevent="parseFormFields"
-          type="text"
-          placeholder="Country"
-          class="input is-small"
-        >
-      </div>
+      <label class="label">
+        Country
+        <div class="control">
+          <input
+            v-model="form['country']"
+            v-on:keydown.13.prevent="parseFormFields"
+            type="text"
+            placeholder="Country"
+            class="input is-small"
+          >
+        </div>
+      </label>
     </div>
 
     <div class="field">
-      <label class="label">Distance (miles)</label>
-      <div class="field-body is-horizontal">
-        <div class="field">
-          <input
-            v-model="form['distance-min']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="min"
-            class="input is-small"
-          >
+      <label class="label">
+        Distance (miles)
+        <div class="field-body is-horizontal">
+          <div class="field">
+            <input
+              v-model="form['distance-min']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="min"
+              class="input is-small"
+            >
+          </div>
+          <div class="field">
+            <input
+              v-model="form['distance-max']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="max"
+              class="input is-small"
+            >
+          </div>
         </div>
-        <div class="field">
-          <input
-            v-model="form['distance-max']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="max"
-            class="input is-small"
-          >
-        </div>
-      </div>
+      </label>
     </div>
 
     <div class="field">
-      <label class="label">Duration (minutes)</label>
-      <div class="field-body is-horizontal">
-        <div class="field">
-          <input
-            v-model="form['duration-min']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="min"
-            class="input is-small"
-          >
+      <label class="label">
+        Duration (minutes)
+        <div class="field-body is-horizontal">
+          <div class="field">
+            <input
+              v-model="form['duration-min']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="min"
+              class="input is-small"
+            >
+          </div>
+          <div class="field">
+            <input
+              v-model="form['duration-max']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="max"
+              class="input is-small"
+            >
+          </div>
         </div>
-        <div class="field">
-          <input
-            v-model="form['duration-max']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="max"
-            class="input is-small"
-          >
-        </div>
-      </div>
+      </label>
     </div>
 
     <div class="field">
-      <label class="label">Mile Pace (M:SS)</label>
-      <div class="control">
-        <input
-          v-model="form['mile-pace']"
-          v-on:keydown.13.prevent="parseFormFields"
-          type="text"
-          placeholder="Mile Pace"
-          class="input is-small"
-        >
-      </div>
+      <label class="label">
+        Mile Pace (M:SS)
+        <div class="control">
+          <input
+            v-model="form['mile-pace']"
+            v-on:keydown.13.prevent="parseFormFields"
+            type="text"
+            placeholder="Mile Pace"
+            class="input is-small"
+          >
+        </div>
+      </label>
     </div>
 
     <div class="field">
-      <label class="label">Layoff (days)</label>
-      <div class="field-body is-horizontal">
-        <div class="field">
-          <input
-            v-model="form['layoff-min']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="Layoff (min)"
-            class="input is-small"
-          >
+      <label class="label">
+        Layoff (days)
+        <div class="field-body is-horizontal">
+          <div class="field">
+            <input
+              v-model="form['layoff-min']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="Layoff (min)"
+              class="input is-small"
+            >
+          </div>
+          <div class="field">
+            <input
+              v-model="form['layoff-max']"
+              v-on:keydown.13.prevent="parseFormFields"
+              type="text"
+              placeholder="max"
+              class="input is-small"
+            >
+          </div>
         </div>
-        <div class="field">
-          <input
-            v-model="form['layoff-max']"
-            v-on:keydown.13.prevent="parseFormFields"
-            type="text"
-            placeholder="max"
-            class="input is-small"
-          >
-        </div>
-      </div>
+      </label>
     </div>
 
     <div class="field">
